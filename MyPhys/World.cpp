a10fe9117f5dda392548b57541a7578830c375d1
Sahil Bhalla <s2bhalla@UCSD.EDU>
2021-01-07 12:18:44 -0800

    Line Endings for List

--- MyPhys/World.cpp
+++ MyPhys/World.cpp
@@ -1,18 +1,12 @@
 #include "World.h"
 
-World::World (float air, void (*forceField)(GameObject *), float time):
+World::World(void (*forceField)(GameObject*), float air, float time) :
 	air_resistance(air), forceField(forceField), time_step(time)
 {
 	GameObjects = new List();
 }
 
-World::World (float air, void (*forceField)(GameObject *)):
-	air_resistance(air), forceField(forceField), time_step(100)
-{
-	GameObjects = new List();
-}
-
-void World::updateField(void(*forceField)(GameObject *))
+void World::updateField(void(*forceField)(GameObject*))
 {
 	this->forceField = forceField;
 }
@@ -20,14 +14,14 @@ void World::updateField(void(*forceField)(GameObject *))
 void World::forces()
 {
 	for (int i = 0; i < GameObjects->NumElements(); ++i) {
-		GameObject * curr = GameObjects->rotate();
+		GameObject* curr = GameObjects->rotate();
 		forceField(curr);
 	}
 }
 
-GameObject * World::add(GameObject * o)
+GameObject* World::add(GameObject* o)
 {
-	return ((GameObject *) GameObjects->Insert(o, END));
+	return ((GameObject*)GameObjects->Insert(o, END));
 }
 
 void World::next()
